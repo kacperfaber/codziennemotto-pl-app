1a17c2d6ceffb1c2985f09db1c5e57f2f252fd3c
Kacper Faber <kacperf1234@gmail.com>
2023-04-23 12:16:00 +0200

    navbar.ts
    Created Navbar with icon-menu button. Navbar is position:fixed and have vh height.
    
    Navbar has border-bottom.

--- src/ui/components/navbar/navbar.ts
+++ src/ui/components/navbar/navbar.ts
@@ -2,5 +2,10 @@ import m from "mithril";
 import {t} from "i18next";
 
 export const Navbar = () => {
-    return {view: () => m("#app_navbar", m("#app_navbar__logo", t("navbar.title")))};
+    return {
+        view: () => m("#app_navbar",
+            m("button#app_navbar__menu_button.btn", m("span#app_navbar__menu_button__icon.icon.icon-menu")),
+            m("#app_navbar__logo", t("navbar.title"))
+        )
+    };
 }
