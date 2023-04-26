345b5dc905f89e3aef364e899b03817f07d5bf15
Kacper Faber <kacperf1234@gmail.com>
2023-04-26 15:45:50 +0200

    Created TextSetList component.

---
+++ src/ui/components/textSet/list/TextSetList.ts
@@ -0,0 +1,24 @@
+import m, {Vnode} from "mithril";
+import {TextSet} from "../../../../services/textSet/textSet";
+
+
+export interface TextSetListAttrs {
+    items: Array<TextSet>;
+}
+
+export function TextSetListItem() {
+    return {
+        view: (vnode: Vnode<{ item: TextSet }>) => m(".text-set-list-item",
+            m("h5", vnode.attrs.item.title),
+            m("p", vnode.attrs.item.description)
+        )
+    }
+}
+
+export function TextSetList() {
+    return {
+        view: (vnode: Vnode<TextSetListAttrs>) => m(".text-set-list",
+            vnode.attrs.items.map(textSet => m(TextSetListItem, {item: textSet}))
+        )
+    }
+}
