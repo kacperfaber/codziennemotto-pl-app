b6baf4b5b438706bdc9916855447beedd9bb97cf
Kacper Faber <kacperf1234@gmail.com>
2023-04-26 14:48:59 +0200

    Added TextSetService 'getMine' and 'getNotMine'

--- src/services/textSet/textSetService.ts
+++ src/services/textSet/textSetService.ts
@@ -1,11 +1,24 @@
 import {TextSet} from "./textSet";
 import {withTokenAsync} from "../withToken";
 import {TextSetApi} from "../../api/textSet/textSetApi";
+import {TextSetStore} from "../../store/textSet/textSetStore";
 
 export class TextSetService {
     static async getById(id: number): Promise<TextSet> {
        return withTokenAsync(
            token => TextSetApi.getById(id, token)
        );
+    }
+
+    static async getMine(): Promise<Array<TextSet>> {
+        return withTokenAsync(TextSetApi.getMine);
+    }
+
+    static async getNotMine(): Promise<Array<TextSet>> {
+        return withTokenAsync(async (token) => {
+            const mine = await TextSetApi.getNotMine(token);
+            TextSetStore.setMine(mine);
+            return mine;
+        });
     }
 }
