0431935537164880c56cde08f3d6f8dfdb7af048
Kacper Faber <kacperf1234@gmail.com>
2023-04-26 12:14:35 +0200

    Created SideNavLinks component.

---
+++ src/ui/components/sidenav/sideNavLinks.ts
@@ -0,0 +1,30 @@
+import m, {Vnode} from "mithril";
+import {Links} from "../../../routes";
+import {t} from "i18next";
+import route from "mithril/route";
+
+type Link = { route: string, text: string };
+
+const LinkList: Array<Link> = [
+    {route: Links.home, text: "all.home"}
+];
+
+interface SideNavLinkAttrs {
+    link: Link
+}
+
+const SideNavLink = {
+    view: (vnode: Vnode<SideNavLinkAttrs, never>) => m(".app-sidenav-link",
+        m("button.btn.app-sidenav-link-button", {onclick: () => route.set(vnode.attrs.link.route)}, t(vnode.attrs.link.text))
+    )
+}
+
+export const SideNavLinks = function () {
+    return {
+        view: () => m("#app_sidenav__links",
+            LinkList.map(link => {
+                return m(SideNavLink, {link: link})
+            })
+        )
+    }
+}
