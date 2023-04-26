9e51a978f0a866b86ecd352fccec07f44d19f044
Kacper Faber <kacperf1234@gmail.com>
2023-04-26 14:19:07 +0200

    Set SideNavCtrl in navbar.ts->menu button.

--- src/ui/components/navbar/navbar.ts
+++ src/ui/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import m from "mithril";
 import {t} from "i18next";
+import {SideNavCtrl} from "../sidenav/sideNavCtrl";
 
 export const Navbar = () => {
     return {
         view: () => m("#app_navbar",
-            m("button#app_navbar__menu_button.btn", m("span#app_navbar__menu_button__icon.icon.icon-menu")),
+            m("button#app_navbar__menu_button.btn", {onclick: SideNavCtrl.open} ,m("span#app_navbar__menu_button__icon.icon.icon-menu")),
             m("#app_navbar__logo", t("navbar.title"))
         )
     };
