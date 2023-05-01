abd139913e5ef4aea73ae54c4c8047cf484458bb
Kacper Faber <kacperf1234@gmail.com>
2023-05-01 13:58:34 +0200

    Created expandableTextList.ts
    It shows component with list of items, title {if pass} and show-all button [if callback pass]

---
+++ src/ui/components/textSet/textList/expandable/expandableTextList.ts
@@ -0,0 +1,42 @@
+import m, {Vnode} from "mithril";
+import {Text} from "../../../../../services/textSet/text";
+import route from "mithril/route";
+import {Links} from "../../../../../routes";
+import {t} from "i18next";
+
+export function ExpandableTextListItem() {
+    return {
+        view: (vnode: Vnode<{ item: Text }>) => m("button.btn.expandable-text-list-item",
+            {onclick: () => route.set(Links.textById(vnode.attrs.item.id))},
+            m("p.text", vnode.attrs.item.text),
+            m("p.id", `${t("all.id")}: ${vnode.attrs.item.id}`)
+        )
+    }
+}
+
+export interface ExpandableTextListAttrs {
+    items: Array<Text>;
+    /**
+     * @description If undefined, title wouldn't render.
+     */
+    title: string | undefined;
+    /**
+     * @description If undefined, 'show-all' button wouldn't render.
+     */
+    showAllOnClick: () => void | undefined;
+}
+
+export function ExpandableTextList() {
+    return {
+        view: (vnode: Vnode<ExpandableTextListAttrs>) => m(".expandable-text-list",
+            vnode.attrs.title ? m("h4", vnode.attrs.title!!) : m("div"),
+
+            vnode.attrs.items.map(item => m(ExpandableTextListItem, {item: item})),
+
+            vnode.attrs.showAllOnClick ?
+                m(".expandable-text-list-footer",
+                    m("button.btn.btn-primary", {onclick: vnode.attrs.showAllOnClick}, t("all.show_all"))
+                ) : m("div")
+        )
+    };
+}
