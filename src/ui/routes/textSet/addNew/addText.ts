7dda43ba6fe40f8e1fbe5e70f7879bf5faf05329
Kacper Faber <kacperf1234@gmail.com>
2023-05-09 15:58:05 +0200

    Finished the addText.ts component.

--- src/ui/routes/textSet/addNew/addText.ts
+++ src/ui/routes/textSet/addNew/addText.ts
@@ -5,6 +5,9 @@ import m from "mithril";
 import {t} from "i18next";
 import {Text} from "../../../../services/textSet/text";
 import {TextSetService} from "../../../../services/textSet/textSetService";
+import {LiveTextSetPreview} from "../../../components/textSet/textSetPreview/live/liveTextSetPreview";
+
+// TODO: No support for 'date' and 'order' properties.
 
 export interface AddTextAttrs {
     textSetId: number;
@@ -15,8 +18,6 @@ export function AddText(vnode: Vnode<AddTextAttrs>): Mithril.Component<AddTextAt
 
     function onSubmit(ev: Event) {
         ev.preventDefault();
-
-        console.log("submitting form with " + JSON.stringify({...state, ...vnode.attrs}));
 
         TextSetService.createText(vnode.attrs.textSetId, state.text, state.date, state.order)
             .then(onSuccess)
@@ -31,36 +32,46 @@ export function AddText(vnode: Vnode<AddTextAttrs>): Mithril.Component<AddTextAt
         let buttonClassList = requireButton().classList;
         buttonClassList.add("btn-success");
         buttonClassList.remove("btn-primary");
-        console.log("success");
     }
 
     function onFailed() {
         let buttonClassList = requireButton().classList;
         buttonClassList.add("btn-danger");
         buttonClassList.remove("btn-primary");
-        console.log("failed");
     }
 
     function onTextChanged(ev: Event) {
         state.text = (ev.target as HTMLInputElement).value;
-        console.log("text changed " + state.text);
     }
 
     return new class extends BaseComponent<AddTextAttrs, any> {
         override view(vnode: Vnode<AddTextAttrs>): Mithril.Children | void | null {
             return Layout.free(
                 m("#app_add_text",
-                    Layout.center(
-                        m("form#app_add_text__form", {onsubmit: onSubmit},
-                            m(".form-floating",
-                                m("input.form-control#app_add_text__form__input_text[type=text]", {
-                                    onchange: onTextChanged,
-                                    placeholder: t("text-set.add-text.input_text.placeholder")
-                                }),
-                                m("label", {'for': 'app_add_text__form__input_text'}, t("text-set.add-text.input_text.label"))
-                            ),
+                    Layout.centerNodes(
+                        Layout.withHeader(
 
-                            m("button#app_add_text__form__submit.btn.btn-primary", {type: 'submit'}, t("text-set.add-text.submit"))
+                            t("text-set.add-text.header"),
+
+                            undefined,
+
+                            m("form#app_add_text__form", {onsubmit: onSubmit},
+                                m(".form-floating",
+                                    m("input.form-control#app_add_text__form__input_text[type=text]", {
+                                        onchange: onTextChanged,
+                                        placeholder: t("text-set.add-text.input_text.placeholder")
+                                    }),
+                                    m("label", {'for': 'app_add_text__form__input_text'}, t("text-set.add-text.input_text.label"))
+                                ),
+
+                                m("button#app_add_text__form__submit.btn.btn-primary", {type: 'submit'}, t("text-set.add-text.submit"))
+                            )
+                        ),
+
+                        Layout.withHeader(
+                            t("text-set.preview.this-is-preview"),
+                            undefined,
+                            m(LiveTextSetPreview, {textSetId: vnode.attrs.textSetId})
                         )
                     )
                 )
