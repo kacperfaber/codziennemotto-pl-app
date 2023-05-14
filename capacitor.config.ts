2aa11062bff1b0c0156eb2fc3f1c6fcb601d56bc
Kacper Faber <kacperf1234@gmail.com>
2023-05-14 12:14:01 +0200

    Added @capacitor, with platform 'android'. It's working.

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pl.codziennemotto',
+  appName: 'codziennemotto.pl',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
