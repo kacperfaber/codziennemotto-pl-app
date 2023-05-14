727eb35f5f8cf38393a0888fbaa250ac6e77860f
Kacper Faber <kacperf1234@gmail.com>
2023-05-14 12:51:37 +0200

    webpack - copying bootstrap-night from node_modules.
    Changed bundle destination, from www/dist/compile-js to www/generated/compile-js.
    
    bootstrap-night.css copying with webpack from node_modules to www/generated/css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         main: "./src/index.ts",
     },
     output: {
-        path: path.resolve(__dirname, './www/dist/compile-js/'),
+        path: path.resolve(__dirname, './www/generated/compile-js/'),
         filename: "app-bundle.js"
     },
     resolve: {
@@ -16,7 +17,26 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 loader: "ts-loader"
+            },
+
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
         ]
-    }
+    },
+
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'node_modules/bootstrap-dark-5/dist/css/bootstrap-night.css',
+                    to: path.resolve(__dirname, 'www/generated/css/bootstrap-night.css')
+                }
+            ]
+        })
+    ]
 };
