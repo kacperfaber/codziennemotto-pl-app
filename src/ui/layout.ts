fa2bcc67166715a7e55dedb31b490fe170c9af82
Kacper Faber <kacperf1234@gmail.com>
2023-05-18 17:38:56 +0200

    Added DialogContainer to Layout.free().

--- src/ui/layout.ts
+++ src/ui/layout.ts
@@ -2,13 +2,15 @@ import m, {Vnode} from "mithril";
 import {Navbar} from "./components/navbar/navbar";
 import {SideNav} from "./components/sidenav/sideNav";
 import Mithril from "mithril";
+import {DialogContainer} from "./base/dialog/dialogContainer";
 
 export class Layout {
     static free(node: Vnode<any, any>): Vnode<any, any> {
         return m("#app",
             m(Navbar),
             m(SideNav),
-            m("#app_container.animate-in", node)
+            m("#app_container.animate-in", node),
+            m(DialogContainer),
         );
     }
 
