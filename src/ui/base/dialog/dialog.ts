be88459dda8a9749724d1caddb080cd77dec81bd
Kacper Faber <kacperf1234@gmail.com>
2023-05-20 10:22:51 +0200

    Created base dialog called DialogComponent.
    DialogComponent it's just a Mithril.Component with a 'DialogController', DialogController it's an interface with 'close' method.

---
+++ src/ui/base/dialog/dialog.ts
@@ -0,0 +1,36 @@
+import Mithril from "mithril";
+import {DialogManager} from "./dialogManager";
+
+export interface DialogController {
+    close: () => void;
+}
+
+export class DialogComponent implements Mithril.Component<any, any> {
+    onbeforeremove(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): Promise<any> | void {
+        return undefined;
+    }
+
+    onbeforeupdate(vnode: Mithril.Vnode<any, Mithril._NoLifecycle<any>>, old: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): boolean | void {
+        return undefined;
+    }
+
+    oncreate(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): any {
+    }
+
+    oninit(vnode: Mithril.Vnode<any, Mithril._NoLifecycle<any>>): any {
+    }
+
+    onremove(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): any {
+    }
+
+    onupdate(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): any {
+    }
+
+    view(vnode: Mithril.Vnode<any, Mithril._NoLifecycle<any>>): Mithril.Children | void | null {
+        return null;
+    }
+
+    protected ctrl: DialogController = {
+        close: () => DialogManager.close(this)
+    }
+}
