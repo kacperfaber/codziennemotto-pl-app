730f431daa05ad63725590d90855563e1cfe4f82
Kacper Faber <kacperf1234@gmail.com>
2023-05-20 10:48:53 +0200

    dialogFrame.ts - I forget the m(Component) instead Component.

--- src/ui/base/dialog/dialogFrame.ts
+++ src/ui/base/dialog/dialogFrame.ts
@@ -4,6 +4,6 @@ import m from "mithril";
 
 export function DialogFrame(vnode: Vnode<{dialog: DialogComponent}>): Mithril.Component<{dialog: DialogComponent}> {
     return {
-        view: () => m(".dialog-frame", vnode.attrs.dialog)
+        view: () => m(".dialog-frame", m(vnode.attrs.dialog))
     }
 }
