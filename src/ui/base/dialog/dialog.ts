835a8a094e9dcbce5e2f63e51ff528da0930cd97
Kacper Faber <kacperf1234@gmail.com>
2023-05-20 12:23:47 +0200

    in DialogComponent class removed unused override members

--- src/ui/base/dialog/dialog.ts
+++ src/ui/base/dialog/dialog.ts
@@ -18,25 +18,6 @@ export class DialogComponent implements Mithril.Component<any, any> {
     constructor(public attrs: any) {
     }
 
-    onbeforeremove(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): Promise<any> | void {
-        return undefined;
-    }
-
-    onbeforeupdate(vnode: Mithril.Vnode<any, Mithril._NoLifecycle<any>>, old: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): boolean | void {
-        return undefined;
-    }
-
-    oncreate(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): any {
-    }
-
-    oninit(vnode: Mithril.Vnode<any, Mithril._NoLifecycle<any>>): any {
-    }
-
-    onremove(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): any {
-    }
-
-    onupdate(vnode: Mithril.VnodeDOM<any, Mithril._NoLifecycle<any>>): any {
-    }
 
     view(vnode: Mithril.Vnode<any, Mithril._NoLifecycle<any>>): Mithril.Children | void | null {
         return null;
@@ -49,4 +30,8 @@ export class DialogComponent implements Mithril.Component<any, any> {
     protected ctrl: DialogController = {
         close: () => DialogManager.close(this)
     }
+}
+
+export class AwaitableDialogComponent extends DialogComponent {
+
 }
